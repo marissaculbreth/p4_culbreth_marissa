f85656d9fc3a9627208de2e48024960f2331caf0
marissaculbreth <50034168+marissaculbreth@users.noreply.github.com>
2019-08-07 09:33:35 -0400

    Javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById("image").addEventListener("mouseenter",
+  function() {
+    alert("Have you been stung before? Find out what to do.");
+  }, false
+);
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
